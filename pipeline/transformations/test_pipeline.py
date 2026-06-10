bc3e0b2a29ab0f2129b2678b1d4f343dc062b475
mashhood <mashhoodhamid201@gmail.com>
2026-06-10 08:38:56 -0700

    Check Pipeline Logs

--- pipeline/transformations/test_pipeline.py
+++ pipeline/transformations/test_pipeline.py
@@ -3,9 +3,6 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.getActiveSession()
 
-for i in range(10):
-    print(i)
-
 @dp.table
 def sample_aggregation_jun_10_redeploy_test():
     return spark.range(10)
